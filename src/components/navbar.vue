66443a3b112a282f0ab0788ae95d0d43779e42cf
mashoun <mashoun.me@gmail.com>
2023-09-29 12:37:52 +0300

    Added the sheet links

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -11,6 +11,9 @@
       <router-link to="/workshops">Workshops</router-link>
       <router-link to="/fees">Fees</router-link>
       <router-link to="/certifications">Certifications</router-link>
+      <a href="https://docs.google.com/spreadsheets/d/19wDLCl783n421nW1wzut2EJlWwWqIjRqMsYlHuO8X8M/edit?usp=sharing">IAU Applications</a>
+      <a href="https://docs.google.com/spreadsheets/d/1W-y0wXuDJ0PNHCwvfxPHocGAW-s3MGjBTX9P7mS5AUo/edit?usp=sharing">Students Applications</a>
+      <a href="https://docs.google.com/spreadsheets/d/10WQBdEdc299tTJ_DfQ8RUDeke95nNCgq_nLvHllX4y0/edit?usp=sharing">Teachers Applications</a>
     </nav>
     <i class="d-block d-lg-none bi bi-three-dots-vertical fs-4 text-secondary" data-bs-toggle="dropdown"></i>
     <ul class="dropdown-menu  nav-links">
@@ -21,6 +24,9 @@
         <li class="dropdown-item"><router-link to="/workshops">Workshops</router-link></li>
         <li class="dropdown-item"><router-link to="/fees">Fees</router-link></li>
         <li class="dropdown-item"><router-link to="/certifications">Certifications</router-link></li>
+        <a href="https://docs.google.com/spreadsheets/d/19wDLCl783n421nW1wzut2EJlWwWqIjRqMsYlHuO8X8M/edit?usp=sharing">IAU Applications</a>
+        <a href="https://docs.google.com/spreadsheets/d/1W-y0wXuDJ0PNHCwvfxPHocGAW-s3MGjBTX9P7mS5AUo/edit?usp=sharing">Students Applications</a>
+        <a href="https://docs.google.com/spreadsheets/d/10WQBdEdc299tTJ_DfQ8RUDeke95nNCgq_nLvHllX4y0/edit?usp=sharing">Teachers Applications</a>
     </ul>
   </header>
 </template>
